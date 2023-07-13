0cf21eac665a0f5a92ad91d9339a3afcffc3a144
João Carllos <jocasilvalima@gmail.com>
2023-07-13 12:45:43 -0300

    Commit exrcícios 4, 5 e 6

---
+++ Lista05/src/Questao05.java
@@ -0,0 +1,12 @@
+
+public class Questao05 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <=10; i++) {
+			System.out.println("2 ^ " + i + " = " + Math.pow(2, i));
+		}
+
+	}
+
+}
